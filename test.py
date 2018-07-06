742b3e697922eaca2a9e725570672d56154ade49
Aleksandr Butolin <alexbutol@yandex.ru>
2018-07-06 13:25:20 +0300

    visual test

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import numpy as np
 import mayavi
 from mayavi import mlab
 
-x, y, z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
-s = np.sin(x*y*z)/(x*y*z)
+s = np.load('data/pr772_fourier_space.npy')
+
 
 src = mlab.pipeline.scalar_field(s)
 mlab.pipeline.iso_surface(src, contours=[s.min()+0.1*s.ptp(), ], opacity=0.3)
