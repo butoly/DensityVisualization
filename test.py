78fdd322d64db9f9499657523a1b88b503105dd8
Aleksandr Butolin <alexbutol@yandex.ru>
2018-07-10 17:36:56 +0300

    cube

--- test.py
+++ test.py
@@ -1,13 +1,48 @@
+'''
 import numpy as np
 import mayavi.mlab as mlab
 
-# Create some random data
-N = 20
-x, y, z = np.mgrid[-5:5:30j, -5:5:30j, -5:5:30j]
-s = np.sin(x * y  * z)/(x * y * z)
+x, y, z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
+#s = np.sin(x*y*z)/(x*y*z)
+s = x**2 + y**2 + z**2
 
-# Plot and show in mayavi2
-#pts = mlab.points3d(x, y, z)#, val, scale_factor=.5,transparent=True)
-#mlab.pipeline.volume(x,y,z)
-mlab.pipeline.volume(mlab.pipeline.scalar_field(s))
+
+#scalar = np.load("data/pr772_full_reconst.npy")
+#print(s.shape) Возвращает размерности объекта
+    
+#mlab.pipeline.volume(mlab.pipeline.scalar_field(s), vmin = 0, vmax = 0.8)
+mlab.contour3d(x, y, z, s)
+
+mlab.show()
+'''
+
+
+import numpy as np
+from scipy import stats
+from mayavi import mlab
+
+mu, sigma = 0, 0.1 
+x = 10*np.random.normal(mu, sigma, 5000)
+y = 10*np.random.normal(mu, sigma, 5000)    
+z = 10*np.random.normal(mu, sigma, 5000)
+
+xyz = np.vstack([x,y,z])
+kde = stats.gaussian_kde(xyz)
+
+# Evaluate kde on a grid
+xmin, ymin, zmin = x.min(), y.min(), z.min()
+xmax, ymax, zmax = x.max(), y.max(), z.max()
+xi, yi, zi = np.mgrid[xmin:xmax:30j, ymin:ymax:30j, zmin:zmax:30j]
+coords = np.vstack([item.ravel() for item in [xi, yi, zi]]) 
+density = kde(coords).reshape(xi.shape)
+
+# Plot scatter with mayavi
+figure = mlab.figure('DensityPlot')
+
+grid = mlab.pipeline.scalar_field(xi, yi, zi, density)
+min = density.min()
+max=density.max()
+mlab.pipeline.volume(grid, vmin=min, vmax=min + .5*(max-min))
+
+mlab.axes()
 mlab.show()
--- test3.py
+++ test3.py
@@ -1,19 +1,16 @@
+### Визуализация плотности с равномерной сеткой плотности (плотности точек одинаковы)
+
 import numpy as np
-from numpy import sin, cos, pi
-from skimage import measure
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import mayavi.mlab as mlab
 
+x, y ,z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
+s = x + y + z
+index = [[[10,10,10]]]
+new_s = np.delete(s, index)
+#s = np.sin(x*y*z)/(x*y*z)
+#print(s)
+mlab.pipeline.volume(mlab.pipeline.scalar_field(new_s), color = (0,1,1))
+#mlab.contour3d(s)
 
-def fun(x, y, z):
-    return cos(x) + cos(y) + cos(z)
+mlab.show()
 
-x, y, z = pi*np.mgrid[-1:1:31j, -1:1:31j, -1:1:31j]
-vol = fun(x, y, z)
-verts, faces = measure.marching_cubes(vol, 0, spacing=(0.1, 0.1, 0.1))
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_trisurf(verts[:, 0], verts[:,1], faces, verts[:, 2],
-                cmap='Spectral', lw=1)
-plt.show()
